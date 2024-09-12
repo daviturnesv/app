4a995b5fa779d2f0e96e985ae5318f0283a8762d
daviturnesv <davi.turnesv@gmail.com>
2024-09-12 14:39:14 -0300

    arrays, objects & functions

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
-// olá. mundo
+// arrays, objetos
+let meta = {
+    value: "ler um livro todo mês",
+    checked: false,
+    log: (info) => {
+        console.log(info)
+    }
+}
 
-let mensage = "konichiwa, sekai"
-console.log("Mensagem original: " + mensage)
-mensage = "olá, mundo"
-console.log("Mensagem traduzida: " + mensage)
+// function // arrow function // named function
+
+function alterarMeta(novaMeta) { 
+    meta.value = novaMeta
+    console.log("* Meta Alterada *")
+    console.log("Nova meta: " + meta.value)
+}
+
+// Função Main
+console.log("Meta Atual: "+ meta.value)
+alterarMeta("Correr 5km todo dia")
